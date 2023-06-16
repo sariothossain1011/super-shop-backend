817b1fa39a09020e46c6bc40110170126d72b838
Sariot Hossain <106438276+sariothossain1011@users.noreply.github.com>
2023-06-16 23:48:06 +0600

    port remove in env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app")
 
-const PORT =  process.env.PORT || 8081 ;
+const PORT =  8080 ;
 
 app.listen(PORT,()=>{
     console.log(`Server is runing at http://localhost:${PORT}`)
